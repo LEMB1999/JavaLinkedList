e3c7a71c6b7e59de9ac72b258b10e90d76cd888f
Luis Emmanuel Mendez Barrios <mendezemmanuel1999@gmail.com>
2020-03-20 15:35:11 -0600

    linkedList

---
+++ src/linkedlist/ClassLinkedList.java
@@ -0,0 +1,175 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlist;
+
+/**
+ *
+ * @author esnai
+ */
+public class ClassLinkedList {
+    private Node head;
+    private Node tail;
+    public ClassLinkedList(){
+        this.head=null;
+        this.tail=null;
+    }
+    
+    public void pushFront(int valor){
+        if(head==null){
+            head=new Node(valor);
+        }else{
+            Node node= new Node(valor);
+            node.setSig(head);
+            head=node;
+        }
+    }
+    
+    public int popFront(){
+        int valor=0;
+        if(head==null){
+            System.out.println("La lista esta Vacia");
+        }else{
+            Node aux=head;
+            valor=aux.getValor();
+            head=head.getSig();
+        }
+      return valor;
+    }
+    
+    public int topFront(){
+        if(head==null){
+            System.out.println("La lista esta Vacia");
+            return 0;
+        }else{
+            return head.getValor();
+        }
+            
+    }
+    
+    public boolean isEmpty(){
+        return head==null;
+    }
+    
+    public int countElements(){
+        int valor=0;
+        if(head==null){
+            valor=0;
+        }else{
+            valor=1;
+            Node aux=head;
+            while(aux.getSig()!=null){
+                aux=aux.getSig();
+                valor++;
+            }
+        }
+        return valor;
+    }
+    
+    //without tail
+    public void pushBack(int valor){
+        if(head!=null){
+            Node aux=head;
+            while(aux.getSig()!=null){
+                aux=aux.getSig();
+            }
+            aux.setSig(new Node(valor));
+        }else{
+            head=new Node(valor);
+        }
+    }
+    
+    public int topBack(){
+        int valor=0;
+        if(head!=null){
+            Node aux=head;
+            while(aux.getSig()!=null){
+                aux=aux.getSig();
+            }
+         valor=aux.getValor();
+        }else
+            System.out.println("La lisa esta Vacia");
+        return valor;
+    }
+    
+    public int popBack(){
+        int valor=0;
+        if(head==null){
+            System.out.println("La lista esta Vacia");
+        }else{
+            Node aux=head;
+            while(aux.getSig().getSig()!=null){
+                  aux=aux.getSig();
+            }
+            valor=aux.getSig().getValor();
+            aux.setSig(null);
+        }
+        return valor;
+    }
+    
+    
+    public void Erase(int key){
+        if(head==null){
+            System.out.println("La lista esta vacia");
+        }else{
+                Node aux=head;
+                Node auxAnterior=head;
+                
+                if(head.getValor()==key){
+                    head=head.getSig();
+                }else{
+                         while(aux.getSig()!=null){    
+                               auxAnterior=aux;
+                               aux=aux.getSig();
+                               if(aux.getValor()==key){
+                                  auxAnterior.setSig(aux.getSig());
+                                  }
+                        }
+            }
+     }
+}
+    
+     
+    public boolean find(int key){
+        boolean estado=false;
+        if(head==null){
+            System.out.println("La lista esta Vacia");
+        }else{
+                Node aux=head;
+                do{
+                    if(aux.getValor()==key){
+                        estado=true;
+                        break;
+                    }
+                     aux=aux.getSig();
+                }while(aux!=null);
+             }
+            return estado;
+        }
+    
+    
+    
+    public void addBefore(){
+        
+    }
+    public void addAfter(){
+        
+    }
+    
+    
+    public void MostrarLista(){
+        if(head==null){
+            System.out.println("La lista esta Vacia");
+        }else{
+            Node aux=head;
+            do{
+                System.out.print(aux.getValor()+" ");
+                aux=aux.getSig();
+            }    
+            while(aux!=null);
+            System.out.println("");
+        }
+    }
+}
